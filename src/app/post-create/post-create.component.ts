00df31dac36f2b00e625e11741b449a8e310ce8b
Travis Stark <tstark93@live.com>
2018-10-06 13:40:31 -0400

    Edit post-create

--- src/app/post-create/post-create.component.ts
+++ src/app/post-create/post-create.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent implements OnInit {
+export class PostCreateComponent {
+  onAddPost() {
 
-  constructor() { }
-
-  ngOnInit() {
   }
-
 }
