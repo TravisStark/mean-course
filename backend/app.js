bf3bc2868eaa69744a2a21289ab09d323cbbd74f
Travis Stark <tstark93@live.com>
2018-11-21 18:14:35 -0500

    Updates

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -16,6 +17,7 @@ mongoose.connect("mongodb+srv://travis:W5ZBonZu5CtX2XtE@cluster0-cx9je.mongodb.n
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
   res.setHeader(
