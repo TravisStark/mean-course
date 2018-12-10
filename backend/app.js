84c1758314352110fbd9fc51cabeda355326b929
Travis Stark <tstark93@live.com>
2018-12-09 21:26:09 -0500

    Updates to auth

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
@@ -37,7 +37,6 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/posts", postsRoutes);
-app.use("/api/user", userRoutes);
 app.use("/api/user", userRoutes);
 
 module.exports = app;
